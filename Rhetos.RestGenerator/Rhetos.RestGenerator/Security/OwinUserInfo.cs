657db7fbbc473db85394dfe2771ce0fd43c8a44a
2CS\hnguyen <hnguyen@2clicksolutions.onmicrosoft.com>
2017-03-23 16:34:05 +0700

    - add owinUserinfo to check recognization. - add code to register "OwinUserInfo" in builder

---
+++ Rhetos.RestGenerator/Rhetos.RestGenerator/Security/OwinUserInfo.cs
@@ -0,0 +1,61 @@
+﻿using Rhetos.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Rhetos.Security;
+using System.Security.Principal;
+
+namespace Rhetos.RestGenerator.Security
+{
+    class OwinUserInfo : IUserInfo
+    {
+        private string _workstation;
+        public OwinUserInfo(IWindowsSecurity windowsSecurity)
+        {
+            _workstation = windowsSecurity.GetClientWorkstation();
+        }
+        public bool IsUserRecognized
+        {
+            get
+            {
+                IIdentity identity = Thread.CurrentPrincipal.Identity;
+                return (
+                    Thread.CurrentPrincipal != null
+                    && Thread.CurrentPrincipal.Identity != null
+                    && Thread.CurrentPrincipal.Identity.IsAuthenticated);
+            }
+        }
+
+        public string UserName
+        {
+            get
+            {
+                CheckIfUserRecognized();
+                return Thread.CurrentPrincipal.Identity.Name;
+            }
+        }
+
+        public string Workstation
+        {
+            get
+            {
+                CheckIfUserRecognized();
+                return _workstation;
+            }
+        }
+
+        public string Report()
+        {
+            return UserName + "," + Workstation;
+        }
+
+        private void CheckIfUserRecognized()
+        {
+            if (!IsUserRecognized)
+                throw new ClientException("User is not authenticated.");
+        }
+    }
+}
